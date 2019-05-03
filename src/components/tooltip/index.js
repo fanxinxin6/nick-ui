2b6803847fceb897b5051f272b709d6b574e84f4
nick <nick@nicks-iMac.local>
2019-05-03 22:31:24 +0800

    tooltip添加enterable

--- src/components/tooltip/index.js
+++ src/components/tooltip/index.js
@@ -4,14 +4,14 @@ import { createFrameworkClass } from '../../utils/index.js'
 import Popover from '../popover/index.js'
 export default {
   name: 'nick-tooltip',
-  props: ['content', 'placement', 'visible', 'openDelay', 'tooltipClass'],
+  props: ['content', 'placement', 'visible', 'openDelay', 'tooltipClass', 'trigger', 'enterable'],
   render () {
     const { prefix } = Theme
     const prefixClass = `${prefix}-tooltip`
     const className = createFrameworkClass({ [prefixClass]: true }, prefix, prefixClass)
-    const { $slots, placement, openDelay, content, tooltipClass } = this
+    const { $slots, placement, openDelay, content, tooltipClass, enterable = false } = this
     return (
-      <Popover popoverClass={`${tooltipClass} ${className}`} placement={placement} openDelay={openDelay} reference={$slots.default}>
+      <Popover popoverClass={`${tooltipClass} ${className} ${prefixClass}-enterable-${enterable}`} placement={placement} openDelay={openDelay} reference={$slots.default}>
         {$slots.content || content}
       </Popover>
     )
